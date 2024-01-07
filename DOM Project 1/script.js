242941512c1bd6b95f75139548d3d0592e14ee4b
Ritesh Kumar <riteshkumarverma2018@gmail.com>
2024-01-07 23:01:37 +0530

    DOM Project Folder is created

---
+++ DOM Project 1/script.js
@@ -0,0 +1,9 @@
+let ele = document.getElementById("div1");
+ele.style.backgroundColor = "yellow";
+ele.style.margin = "20px";
+ele.style.padding = "10px";
+ele.style.fontSize = "18px";
+ele.style.fontWeight = "bold";
+ele.style.height = "200px";
+ele.style.width = "300px";
+ele.style.color = "rgb(52,104,250)"
---
+++ README.md
@@ -0,0 +1,1 @@
+This is the folder for all the projects related to dom manipulation.
